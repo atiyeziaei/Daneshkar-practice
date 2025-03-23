9db4d1de066ba7909edb8686254da1ebd0bc22b7
Atiye <atiyez@yahoo.com>
2025-03-23 16:42:10 +0330

    Add project files.

---
+++ Daneshkar/Practice1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+ class Practice1
+{
+	const string MessageWellcome = "#C to Wellcome";
+
+	static void Main()
+	{
+		int age = 25; 
+		Console.WriteLine(MessageWellcome); 
+		Console.WriteLine("Age: " + age); 
+	}
+}
